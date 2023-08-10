b5778915d5432b02f167f09106dcab43979d7bb9
Prakhar Yadav <yprakher4@gmail.com>
2023-08-10 20:47:56 +0530

    Hello there

---
+++ JavaScript/1/app.js
@@ -0,0 +1,6 @@
+// alert("Hey there ! i m a alert");
+// const ans = prompt("Whats ur Name??","Prakhar");
+// console.log(ans);
+
+const name = prompt("Whats ut name??", "Prakhar");
+document.write("Your name is " + name);
