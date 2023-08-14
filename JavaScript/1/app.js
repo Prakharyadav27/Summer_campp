f99a4f46308c8afcd88e24ebbb3a6d10ca1bde7e
Prakhar Yadav <yprakher4@gmail.com>
2023-08-14 20:03:46 +0530

    Weather app

--- JavaScript/1/app.js
+++ JavaScript/1/app.js
@@ -2,5 +2,8 @@
 // const ans = prompt("Whats ur Name??","Prakhar");
 // console.log(ans);
 
-const name = prompt("Whats ut name??", "Prakhar");
-document.write("Your name is " + name);
+// const name = prompt("Whats ut name??", "Prakhar");
+// document.write("Your name is " + name);
+
+const body = document.querySelector("body");
+body.innerHTML = "<h1>Hacked!!!</h1>";
